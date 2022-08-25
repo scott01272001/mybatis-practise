fdf599c296fecb8ff44d639954dfb897125d7baa
scott <scott01272001@gmail.com>
2022-08-26 00:47:13 +0800

    add log4j2

--- src/main/java/com/scott/dev/mybatispractise/MybatisPractiseApplication.java
+++ src/main/java/com/scott/dev/mybatispractise/MybatisPractiseApplication.java
@@ -1,13 +1,19 @@
 package com.scott.dev.mybatispractise;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MybatisPractiseApplication {
 
+	private static Logger logger = LogManager.getLogger(MybatisPractiseApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(MybatisPractiseApplication.class, args);
+
+		logger.info("log test");
 	}
 
 }
--- src/test/java/com/scott/dev/mybatispractise/test/MyBatisTest.java
+++ src/test/java/com/scott/dev/mybatispractise/test/MyBatisTest.java
@@ -7,15 +7,17 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
 import com.scott.dev.mybatispractise.entity.Account;
 import com.scott.dev.mybatispractise.mapper.AccountMapper;
 
 public class MyBatisTest {
-
     @Test
     public void testMyBatis() throws IOException {
+
         System.out.println("id = ");
         // 1. load mybatis config
         InputStream is = Resources.getResourceAsStream("mybatis/mybatis-config.xml");
@@ -24,14 +26,14 @@ public class MyBatisTest {
         // 3. get sqlSessionFactory
         SqlSessionFactory factory = sqlSessionFactoryBuilder.build(is);
         // 4. get sqlSession
-        SqlSession sqlSession = factory.openSession();
+        SqlSession sqlSession = factory.openSession(true);
         // 5. get mapper impl
         AccountMapper mapper = sqlSession.getMapper(AccountMapper.class);
 
         long id = mapper.insertAccount();
 
         // 要提交事務資料才回實際進入db
-        sqlSession.commit();
+        // sqlSession.commit();
 
         System.out.println("id = " + id);
     }
